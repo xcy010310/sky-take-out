21e9ef026291be32c95971b96928fab500e26486
Sunx <1060448742@qq.com>
2024-12-13 21:02:24 +0800

    苍穹外卖项目v9.0 -- 删除菜品业务代码实现

---
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -0,0 +1,26 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * ClassName: SetmealDishMapper
+ * Package: com.sky.mapper
+ * Description: This is a program for testing.
+ *
+ * @Author Ben
+ * @Create 2024/12/13 20:47
+ * @Version 1.0
+ */
+@Mapper
+public interface SetmealDishMapper {
+    /**
+     * 根据菜品id查询对应的套餐id
+     * @param dishIds
+     * @return
+     */
+    //select setmeal_id from setmeal_dish where dish_id in (1,2,3,4)
+    List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+}
